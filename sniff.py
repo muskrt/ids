82ee1a3630cbd030504a68bd5bd9a38f8975697c
muskrt <kurtmustafa5003@gmail.com>
2020-05-21 21:09:58 +0300

    Update sniff.py

--- sniff.py
+++ sniff.py
@@ -64,108 +64,11 @@ def tst(packet):
 		print(packet.summary())
 
 
-def main(interface):
+def main():
 
 	print('[*] Start Sniffing...')
 	global dataset_Create
 	dataset_Create=1
 	# Thread(target=clock_time).start()
-	sniff(iface=interface,filter='ip',prn=tst)
+	sniff(iface='Wi-Fi',filter='ip',prn=tst)
 	print('[*] Stop sniffing')
-# from scapy.all import *
- 
-# import matplotlib.pyplot as plt
- 
-# import argparse
- 
-# import os
- 
-# parser = argparse.ArgumentParser(description='Live Traffic Examiner')
- 
-# parser.add_argument('interface', help="Network interface to listen on i.e. en0", type=str)
- 
-# parser.add_argument('--count', help="Capture X packets and exit", type=int)
- 
-# args=parser.parse_args()
- 
-# #Check to see if we are root, otherwise Scapy might not be able to listen
- 
-
- 
-# print("Warning: Not running as root, packet listening may not work.")
-
-# try:
-
-#    print("--Trying to listen on {}".format(args.interface))
-
-#    sniff(iface=args.interface,count=1)
-
-#    print("--Success!")
-
-# except:
-
-#    print("--Failed!\nError: Unable to sniff packets, try again using sudo.")
-
-#    quit()
- 
-# if args.count:
- 
-#    print("Capturing {} packets on interface {} ".format(args.count, args.interface))
- 
-# else:
- 
-#    print("Capturing unlimited packets on interface {} \n--Press CTRL-C to exit".format(args.interface))
- 
-# #Interactive Mode
- 
-# plt.ion()
- 
-# #Labels
- 
-# plt.ylabel("Bytes")
- 
-# plt.xlabel("Count")
- 
-# plt.title("Real time Network Traffic")
- 
-# plt.tight_layout()
- 
-# #Empty list to hold bytes
- 
-# yData=[]
- 
-# i=0
- 
-# #Listen indefinitely, or until we reach count
- 
-# while True:
- 
-#    #Listen for 1 packet
- 
-#    for pkt in sniff(iface=args.interface,count=1):
- 
-#        try:
- 
-#            if IP in pkt:
- 
-#                yData.append(len(pkt))
- 
-#                plt.plot(yData)
- 
-#                #Pause and draw
- 
-#                plt.pause(0.1)
- 
-#                i+=1
- 
-#                if args.count:
- 
-#                    if i >= args.count:
- 
-#                        quit()
- 
-#        except KeyboardInterrupt:
- 
-#            print("Captured {} packets on interface {} ".format(i, args.interface))
- 
-#            quit()
